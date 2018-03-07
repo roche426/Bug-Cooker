efab38c19148374d269383c0ad847101febea2f0
Flavien Girier <girier.flavien@gmail.com>
2018-03-07 16:37:13 +0100

    Fichier modifier avec l'ajout Header et Footer

---
+++ header.php
@@ -0,0 +1,13 @@
+<!--Banniere top-->
+		<header>
+
+			<img src="../Images/logo_sans_fond.png" alt="logo_restaurant" id="logo_restaurant_head">
+
+<!--Barre de navigation-->
+				<ul class="nav nav-pills">
+	  			<li><a href="#" class="nav_text">Concept</a></li>
+	  			<li><a href="#" class="nav_text">Notre Carte</a></li>
+	  			<li><a href="#" class="nav_text">Nos Restaurants</a></li>
+				</ul>
+
+		</header>
