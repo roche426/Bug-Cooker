5f28a7f3e4e6b770429cf7e80fb610009613f400
Flavien Girier <girier.flavien@gmail.com>
2018-03-11 21:27:45 +0100

    Branch Flavien

--- footer.php
+++ footer.php
@@ -1,14 +1,39 @@
-<img src="../Images/petit_logo_foot.png" alt="petit_logo" id="logo_restaurant_foot">
+<hr class="ligne_separation">
 
-	<ul class="banniere_bottom">
+<p id="logo_footer">Bug Cookers</p>
+	<ul class="banniere_bottom1">
 		<li><a href="#" class="text_foot">Siège social - 17 Rue Delandine 69002 Lyon</a></li>
 		<li><a href="#" class="text_foot"> Téléphone : 06 58 46 83 32</a></li>
 		<li><a href="#" class="text_foot"> Email : Bug.Cookers@insect.com</a></li>
+	</ul>
+	<ul class="banniere_bottom2">
 		<li><a href="#" class="text_foot">Contact - Mentions légales</a></li>
 	</ul>
 
-	<ul class="banniere_bottom_logo">
-		<li><a href="#"><img src="/Images/logo_facebook.png" alt="logo_facebook" class="taille_logo">Facebook</a></li>
-		<li><a href="#"><img src="/Images/logo_twitter.png" alt="logo_twitter" class="taille_logo">Twitter</a></li>
-		<li><a href="#"><img src="/Images/logo_pinterest.png" alt="logo_pinterest" class="taille_logo">Pinterest</a></li>
+<!--<ul class="banniere_bottom_logo">
+		<li><a href="#"><img src="/Images/logo_facebook.png" alt="logo_facebook" class="taille_logo"></a></li>
+		<li><a href="#"><img src="/Images/logo_twitter.png" alt="logo_twitter" class="taille_logo"></a></li>
+		<li><a href="#"><img src="/Images/logo_pinterest.png" alt="logo_pinterest" class="taille_logo"></a></li>
 	</ul>
+
+<div class="container">
+			<div class="col-md-3">
+			  <p id="logo_footer">Bug Cookers</p>
+		  	</div>
+			<div class="col-md-6">
+				<a href="#" class="text_foot">Siège social - 17 Rue Delandine 69002 Lyon</a>
+				<a href="#" class="text_foot">Téléphone : 06 58 46 83 32</a>
+				<a href="#" class="text_foot">Email : Bug.Cookers@insect.com</a>
+				<a href="#" class="text_foot">Contact</a>
+				<a href="#" class="text_foot">Mentions légales</a>
+			</div>
+			<div class="col-md-1">
+				<a href="#"><img src="/Images/logo_facebook.png" alt="logo_facebook" class="taille_logo"></a>
+			</div>
+			<div class="col-md-1">
+				<a href="#"><img src="/Images/logo_twitter.png" alt="logo_twitter" class="taille_logo"></a>
+			</div>
+			<div class="col-md-1">
+				<a href="#"><img src="/Images/logo_pinterest.png" alt="logo_pinterest" class="taille_logo"></a>
+		  	</div>
+	</div>-->
