dc9a72528b352100dda3c8f1d6628b503ecb70ee
Flavien Girier <girier.flavien@gmail.com>
2018-03-12 09:18:19 +0100

    Mise a jour Branch Flav 12-03

--- footer.php
+++ footer.php
@@ -2,11 +2,11 @@
 
 <p id="logo_footer">Bug Cookers</p>
 	<ul class="banniere_bottom1">
-		<li><a href="#" class="text_foot">Siège social - 17 Rue Delandine 69002 Lyon</a></li>
 		<li><a href="#" class="text_foot"> Téléphone : 06 58 46 83 32</a></li>
 		<li><a href="#" class="text_foot"> Email : Bug.Cookers@insect.com</a></li>
 	</ul>
 	<ul class="banniere_bottom2">
+		<li><a href="#" class="text_foot">Siège social - 17 Rue Delandine 69002 Lyon</a></li>
 		<li><a href="#" class="text_foot">Contact - Mentions légales</a></li>
 	</ul>
 
