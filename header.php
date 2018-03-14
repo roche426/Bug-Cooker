c7bcb09339a4332bac601f55741a2bf1f075211f
Flavien Girier <girier.flavien@gmail.com>
2018-03-14 11:20:01 +0100

    petite modif

--- header.php
+++ header.php
@@ -1,15 +1,12 @@
 <!--Banniere top-->
-		<header>
-
-			<img src="../Images/logo_sans_fond_blanc.png" alt="logo_restaurant" id="logo_restaurant_head">
-
+<header>
+	<img src="../Images/logo_sans_fond_blanc.png" alt="logo_restaurant" id="logo_restaurant_head">
 <!--Barre de navigation-->
-		<hr class="ligne_separation">
-				<ul class="nav nav-pills">
-	  			<li><a href="#" class="nav_text">Concept</a></li>
-	  			<li><a href="#" class="nav_text">Notre Carte</a></li>
-	  			<li><a href="#" class="nav_text">Nos Restaurants</a></li>
-				</ul>
-		<hr class="ligne_separation">
-
-		</header>
+	<hr class="ligne_separation">
+		<ul class="nav nav-pills">
+			<li><a href="#" class="nav_text">Concept</a></li>
+			<li><a href="#" class="nav_text">Notre Carte</a></li>
+			<li><a href="#" class="nav_text">Nos Restaurants</a></li>
+		</ul>
+	<hr class="ligne_separation">
+</header>
