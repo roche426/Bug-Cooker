beeda5bf057094a39eb70835cf5a2bc294661669
Flavien Girier <girier.flavien@gmail.com>
2018-03-16 11:13:04 +0100

    Bientot la fin

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
 <!--Barre de navigation-->
 	<hr class="ligne_separation">
 		<ul id="navigation" class="nav nav-pills">
-			<li<?php if ($nav_en_cours == 'index') {echo ' class="en-cours"';} ?>><a href="index.php" class="nav_text">Concept</a></li>
-			<li<?php if ($nav_en_cours == 'nosmenus') {echo ' class="en-cours"';} ?>><a href="nosMenus.php" class="nav_text">Notre Carte</a></li>
-			<li<?php if ($nav_en_cours == 'nosrestaurants') {echo ' class="en-cours"';} ?>><a href="nosRestaurants.php" class="nav_text">Nos Restaurants</a></li>
+			<li<?php if ($nav_en_cours == 'index') {echo ' id="en-cours"';} ?>><a href="index.php" class="nav_text">Concept</a></li>
+			<li<?php if ($nav_en_cours == 'nosmenus') {echo ' id="en-cours"';} ?>><a href="nosMenus.php" class="nav_text">Notre Carte</a></li>
+			<li<?php if ($nav_en_cours == 'nosrestaurants') {echo ' id="en-cours"';} ?>><a href="nosRestaurants.php" class="nav_text">Nos Restaurants</a></li>
 		</ul>
 	<hr class="ligne_separation">
 
